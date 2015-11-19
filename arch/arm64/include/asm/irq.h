4dd460c37d345f3ef3f27e20b2634124e30a3e2e
Mingkai Hu <Mingkai.Hu@freescale.com>
2015-11-19 15:25:37 +0800

    arm64: add NO_IRQ macro
    Signed-off-by: Mingkai Hu <Mingkai.Hu@freescale.com>

--- arch/arm64/include/asm/irq.h
+++ arch/arm64/include/asm/irq.h
@@ -3,6 +3,14 @@
 
 #include <asm-generic/irq.h>
 
+/*
+ * Use this value to indicate lack of interrupt
+ * capability
+ */
+#ifndef NO_IRQ
+#define NO_IRQ	((unsigned int)(-1))
+#endif
+
 struct pt_regs;
 
 extern void set_handle_irq(void (*handle_irq)(struct pt_regs *));
