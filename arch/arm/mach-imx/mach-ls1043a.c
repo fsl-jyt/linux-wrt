cd8e29fa5a0bf6abd800b557c0576001fa6599bb
Alison Wang <b18965@freescale.com>
2016-05-17 17:23:51 +0800

    armv8: aarch32: Run 32-bit Linux in AArch32 execution state
    This patch adds AArch32 execution state support for LS1043A. Verified
    32-bit Linux kernel can run on LS1043ARDB board.
    
    Signed-off-by: Ebony Zhu <ebony.zhu@nxp.com>
    Signed-off-by: Alison Wang <alison.wang@nxp.com>

---
+++ arch/arm/mach-imx/mach-ls1043a.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2015-2016 Freescale Semiconductor, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <asm/mach/arch.h>
+
+#include "common.h"
+
+static const char * const ls1043a_dt_compat[] __initconst = {
+	"fsl,ls1043a",
+	NULL,
+};
+
+DT_MACHINE_START(LS1043A, "Freescale LS1043A")
+	.dt_compat	= ls1043a_dt_compat,
+MACHINE_END
